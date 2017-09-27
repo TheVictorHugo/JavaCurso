527a141974264ef0fda5efc3604b4b19eaa630cb
Victor Hugo Rodrigues da Silva <victorq32@gmail.com>
2017-09-26 22:12:51 -0300

    HelloWorld

---
+++ Hello World/HelloWorld.java
@@ -0,0 +1,32 @@
+
+class HelloWorld{
+    public static void main (String[] args){
+        int numero = 5;
+        int i = 0;
+        for(i=0; i<10; i = i + 1){
+            System.out.println("Hello World");
+        }
+        i= 0;
+        while(i <10){
+            System.out.println("Ola!");
+            i = i + 1;
+        }
+        // System.out.println("Hello World");
+        int y = 20;
+        int x = 0;
+        
+       // for(int i = x; i < y; i++){
+          //  if(i % 19 == 0){
+            //    System.out.println("Achei um número divissível por 19 entre x e y");
+            //    break;
+           // }
+      //  }
+        
+    }
+}
+/*
+class MinhaClass{
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+}*/
