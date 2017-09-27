4f54affc1d3458a92d08603e8a7a497de7741e69
Victor Hugo Rodrigues da Silva <victorq32@gmail.com>
2017-09-26 22:54:16 -0300

    mais exercicios

---
+++ 06_Aula/Argumentos.java
@@ -0,0 +1,7 @@
+class Argumentos{
+    public static void main(String[] args){
+        System.out.println("Você digitou " + args[0]);
+        String nome = args[0];
+        System.out.println("voce digitou " + nome);
+    }
+}
